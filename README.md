cb46d5cf060ff26a3df3eb1fddfb46d068545eff
Muhammad Fikri Rouzan Ash Shidik <fikrirzn@gmail.com>
2025-02-17 15:42:45 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# StayKos
+
+## About StayKos
+
+StayKos is a boarding house booking website where users can book accommodations online. The main purpose of this website is to make it easier for users to search for and book boarding houses conveniently based on the city or category they are looking for.
+
+## Features
+
+-   Exploring Boarding Houses: Users can explore available boarding houses, view detailed information about them, discover offered bonuses, and read testimonials from other users.
+-   Searching for Boarding Houses: Users can search for boarding houses by entering the name, city, or category of the boarding house they wish to book.
+-   Booking: Users can book a boarding house by filling out a form that includes their full name, email, phone number, and the duration of their stay.
+-   Payment: Users can decide whether to make a down payment or full payment and choose their preferred payment method.
+-   Notification: Users will receive a WhatsApp notification containing booking details after completing the payment.
+-   Check Booking Details: Users can check the details of the boarding house booking they have made by entering the booking ID, email, and phone number they have registered.
+
+## Technologies Used
+
+-   Programming Languages: PHP and JavaScript.
+-   Stylesheet Language: CSS.
+-   Templating Engine: Blade.
+-   Frameworks: Laravel, Filament, and Tailwind CSS.
+-   Database: MySQL.
+-   Payment Gateway: Midtrans.
+-   Tunneling Service: Ngrok.
+-   Notification Service: Twilio.
+-   Software: Visual Studio Code and Laragon.
---
+++ app/Http/Controllers/Api/MidtransController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use Exception;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Http\Request;
+use Twilio\Rest\Client;
+
+class MidtransController extends Controller
+{
+    public function callback(Request $request)
+    {
+        $serverKey = config('midtrans.serverKey');
+        $hashedKey = hash('sha512', $request->order_id . $request->status_code . $request->gross_amount . $serverKey);
+
+        if ($hashedKey !== $request->signature_key) {
+            return response()->json(['message' => 'Invalid signature key'], 403);
+        }
+
+        $transactionStatus = $request->transaction_status;
+        $orderId = $request->order_id;
+        $transaction = Transaction::where('code', $orderId)->first();
+
+        if (!$transaction) {
+            return response()->json(['message' => 'Transaction not found'], 404);
+        }
+
+        $sid    = getenv("TWILIO_ACCOUNT_SID");
+        $token  = getenv("TWILIO_AUTH_TOKEN");
+        $twilio = new Client($sid, $token);
+
+        $message =
+            "Hello, " . $transaction->name . "!" . PHP_EOL . PHP_EOL .
+            "We have received your payment with the booking code: " . $transaction->code . "." . PHP_EOL .
+            "Total payment: Rp " . number_format($transaction->total_amount, 0, ',', '.') . PHP_EOL . PHP_EOL .
+            "You can come to the boarding house: " . $transaction->boardingHouse->name . PHP_EOL .
+            "Address: " . $transaction->boardingHouse->address . PHP_EOL .
+            "Starting date: " . date('d-M-Y', strtotime($transaction->start_date)) . PHP_EOL . PHP_EOL .
+            "Thank you for your trust! 😊" . PHP_EOL .
+            "We look forward to your arrival.";
+
+        switch ($transactionStatus) {
+            case 'capture':
+                if ($request->payment_type == 'credit_card') {
+                    if ($request->fraud_status == 'challenge') {
+                        $transaction->update(['payment_status' => 'pending']);
+                    } else {
+                        $transaction->update(['payment_status' => 'success']);
+                    }
+                }
+                break;
+            case 'settlement':
+                $transaction->update(['payment_status' => 'success']);
+
+                $twilio->messages
+                    ->create(
+                        "whatsapp:+62" . $transaction->phone, // to
+                        array(
+                            "from" => "whatsapp:+14155238886",
+                            "body" => $message
+                        )
+                    );
+
+                break;
+            case 'pending':
+                $transaction->update(['payment_status' => 'pending']);
+                break;
+            case 'deny':
+                $transaction->update(['payment_status' => 'failed']);
+                break;
+            case 'expire':
+                $transaction->update(['payment_status' => 'expired']);
+                break;
+            case 'cancel':
+                $transaction->update(['payment_status' => 'canceled']);
+                break;
+            default:
+                $transaction->update(['payment_status' => 'unknown']);
+                break;
+        }
+
+        return response()->json(['message' => 'Callback received successfully']);
+    }
+}
